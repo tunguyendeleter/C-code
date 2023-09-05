77f42a80faa44f33f47fa4eb4982f36fae068759
tunguyendeleter <tunguyendeleter@gmail.com>
2023-09-05 22:33:08 +0700

    update files

--- C_Chuoi/bieu thuc toan hoc/bieuthuctoanhoc.cpp
+++ C_Chuoi/bieu thuc toan hoc/bieuthuctoanhoc.cpp
@@ -1,52 +1,456 @@
-#include <stdio.h>
-#include <conio.h>
-// #include <string.h>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int STRLEN(char *str)
+int SUPER_STRCMP(string str1, string str2)
 {
+    string temp_str1 = str1;
+    string temp_str2 = str2;
+    int len1 = str1.size();
+    int len2 = str2.size();
+    int min = len1 < len2 ? len1 : len2;
+    int max = len1 > len2 ? len1 : len2;
+    if (min == len1)
+    {
+        for (int i = 0; i < max - min; i++)
+        {
+            temp_str1 = "0" + temp_str1;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < max - min; i++)
+        {
+            temp_str2 = "0" + temp_str2;
+        }
+    }
+    for (int i = 0; i < max; i++)
+    {
+        if (temp_str1[i] > temp_str2[i])
+        {
+            return 1;
+        }
+        else if (temp_str1[i] < temp_str2[i])
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+bool CheckZero(string str)
+{
+    int len = str.size();
+    if (len == 0)
+    {
+        return false;
+    }
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] != '0')
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void CanBangHaiVe(string &str1, string &str2)
+{
+    int str1_len = str1.size();
+    int str2_len = str2.size();
+    int cnt1 = 0;
+    int cnt2 = 0;
+    for (int i = 0; i < str1_len; i++)
+    {
+        if (str1[i] == '.')
+        {
+            for (int j = 0; j < str1_len - i - 1; j++)
+            {
+                str1[i + j] = str1[i + j + 1];
+            }
+            str1.erase(str1_len - 1);
+            cnt1 = str1_len - i - 1;
+            break;
+        }
+    }
+    for (int i = 0; i < str2_len; i++)
+    {
+        if (str2[i] == '.')
+        {
+            for (int j = 0; j < str2_len - i - 1; j++)
+            {
+                str2[i + j] = str2[i + j + 1];
+            }
+            str2.erase(str2_len - 1);
+            cnt2 = str2_len - i - 1;
+            break;
+        }
+    }
+    str1_len = str1.size();
+    str2_len = str2.size();
+    int min = cnt1 < cnt2 ? cnt1 : cnt2;
+    int max = cnt1 > cnt2 ? cnt1 : cnt2;
+    if (min == cnt1)
+    {
+        for (int i = min; i < max; i++)
+        {
+            str1 += "0";
+        }
+    }
+    else
+    {
+        for (int i = min; i < max; i++)
+        {
+            str2 += "0";
+        }
+    }
+}
+
+void ReverseString(string &str)
+{
+    int len = str.size();
+    for (int i = 0; i < len / 2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+}
+
+// copy các toán tử và toán hạng vào vector để xử lý
+void AddVector(string str, vector<string> &a)
+{
+    int len = str.size();
+    string substr = "";
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] != ' ')
+        {
+            substr += str[i];
+        }
+        else if (str[i] == ' ')
+        {
+            a.push_back(substr);
+            substr.clear();
+        }
+        if (i == len - 1)
+        {
+            a.push_back(substr);
+        }
+    }
+}
+
+/*
+  123
+-  96
+------
+   27
+3 - 6 = -3
+if (-3 < 0)
+=> 10+(-3) = 7
+=> nho = 1
+*/
+string PhepTru(string str1, string str2)
+{
+    int str1_len = str1.size();
+    int str2_len = str2.size();
+    int min = str1_len < str2_len ? str1_len : str2_len;
+    int max = str1_len > str2_len ? str1_len : str2_len;
+    if (min == str1_len)
+    {
+        for (int i = 0; i < max - min; i++)
+        {
+            str1 = "0" + str1;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < max - min; i++)
+        {
+            str2 = "0" + str2;
+        }
+    }
+    bool check = false;
+    string temp_str1 = str1;
+    string temp_str2 = str2;
+    if (str1.compare(str2) < 0)
+    {
+        temp_str1 = str2;
+        temp_str2 = str1;
+        check = true;
+    }
+    // cout << "\ncheck = " << check;
+    int remember = 0;
+    string ketqua = "";
+    for (int i = max - 1; i >= 0; i--)
+    {
+        int temp = (temp_str1[i] - 48) - (temp_str2[i] - 48) - remember;
+        if (temp < 0)
+        {
+            ketqua += (10 + temp + 48);
+            remember = 1;
+        }
+        else
+        {
+            ketqua += (temp + 48);
+            remember = 0;
+        }
+    }
+    ReverseString(ketqua);
+    if (check == true)
+    {
+        ketqua = "-" + ketqua;
+    }
+    return ketqua;
+}
+
+/*
+  123
++ 129
+------
+  252
+3 + 9 = 12
+12%10 = 2
+12/10 = 1 = nho
+*/
+string PhepCong(string str1, string str2)
+{
+    int str1_len = str1.size();
+    int str2_len = str2.size();
+    int min = str1_len < str2_len ? str1_len : str2_len;
+    int max = str1_len > str2_len ? str1_len : str2_len;
+    if (min == str1_len)
+    {
+        for (int i = 0; i < max - min; i++)
+        {
+            str1 = "0" + str1;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < max - min; i++)
+        {
+            str2 = "0" + str2;
+        }
+    }
+    int remember = 0;
+    string ketqua = "";
+    for (int i = max - 1; i >= 0; i--)
+    {
+        int temp = (str1[i] - 48) + (str2[i] - 48) + remember;
+        ketqua += (temp % 10 + 48);
+        remember = temp / 10;
+    }
+    ReverseString(ketqua);
+    return ketqua;
+}
+
+/*
+17*3 => 7*3 = 21
+21%10 = 1
+21/10 = 2 => nho
+     => 1*3 = 3 + nho = 5
+*/
+string PhepNhan(string str1, string str2)
+{
+    int len_str1 = str1.size();
+    int len_str2 = str2.size();
+    int remember = 0;
+    string ketqua[len_str2] = "";
+    for (int j = 0; j < len_str2; j++)
+    {
+        for (int i = len_str1 - 1; i >= 0; i--)
+        {
+            int temp = (str1[i] - 48) * (str2[j] - 48) + remember;
+            remember = temp / 10;
+            ketqua[j] += ((temp % 10) + 48);
+        }
+        if (remember > 0)
+        {
+            ketqua[j] += (remember + 48);
+        }
+    }
+    for (int i = 0; i < len_str2; i++)
+    {
+        ReverseString(ketqua[i]);
+        for (int j = 0; j < len_str2 - i - 1; j++)
+        {
+            ketqua[i] += "0";
+        }
+    }
+    string ketquacuoi = "0";
+    for (int i = 0; i < len_str2; i++)
+    {
+        ketquacuoi = PhepCong(ketquacuoi, ketqua[i]);
+    }
+    return ketquacuoi;
+}
+
+/*
+9/4 = 2 du 1
+du 1 = 10/4 = 2 du 2
+du 2 = 20/4 = 5 du 0
+=> 2.25
+*/
+string PhepChia(string str1, string str2)
+{
+    int str1_len = str1.size();
+    int str2_len = str2.size();
+    string sobichia = "";
+    string sochia = "";
+    string ketqua = "";
+    bool first = false;
+    for (int i = 0; i < str1_len; i++)
+    {
+        string heso = "1";
+        sobichia += str1[i];
+        sochia = PhepNhan(str2, heso);
+        bool check = false;
+        while (SUPER_STRCMP(sochia, sobichia) <= 0)
+        {
+            heso[0] += 1;
+            sochia = PhepNhan(str2, heso);
+            if (SUPER_STRCMP(sochia, sobichia) > 0)
+            {
+                first = true;
+                check = true;
+                heso[0] -= 1;
+                sochia = PhepTru(sochia, str2);
+                sobichia = PhepTru(sobichia, sochia);
+                ketqua += heso;
+                break;
+            }
+        }
+        if (first == true && check == false)
+        {
+            ketqua += "0";
+        }
+    }
+    return ketqua;
+}
+
+string PhepChiaThapPhan(string str1, string str2, int num)
+{
+    string str1_temp = str1;
+    string str2_temp = str2;
+    CanBangHaiVe(str1_temp, str2_temp);
+    int str1_len = str1_temp.size();
+    int str2_len = str2_temp.size();
+    string sobichia = "";
+    string sochia = "";
+    string ketqua = "";
+    bool first = false;
     int i = 0;
-    while (str[i] != '\0')
+    int float_num = 0;
+    while (CheckZero(sobichia) == false)
     {
+        string heso = "1";
+        if (i < str1_len)
+        {
+            sobichia += str1_temp[i];
+        }
+        else
+        {
+            float_num++;
+            if (float_num == 1)
+            {
+                ketqua += ".";
+            }
+            sobichia += "0";
+        }
+
+        sochia = PhepNhan(str2_temp, heso);
+        bool check = false;
+        while (SUPER_STRCMP(sochia, sobichia) <= 0)
+        {
+            heso[0] += 1;
+            sochia = PhepNhan(str2_temp, heso);
+            if (SUPER_STRCMP(sochia, sobichia) > 0)
+            {
+                first = true;
+                check = true;
+                heso[0] -= 1;
+                sochia = PhepTru(sochia, str2_temp);
+                sobichia = PhepTru(sobichia, sochia);
+                ketqua += heso;
+                break;
+            }
+        }
+        if (first == true && check == false)
+        {
+            ketqua += "0";
+        }
         i++;
+        if (float_num == num)
+        {
+            break;
+        }
     }
-    return i;
+    return ketqua;
 }
 
-float PhepNhan(char *a, char *b)
+void XuLyNhanChia(vector<string> &a)
 {
-    if ()
+    int len = a.size();
+    for (int i = 0; i < len; i++)
     {
-        /* code */
+        if (a[i] == "*")
+        {
+            string ketqua = PhepNhan(a[i - 1], a[i + 1]);
+            a[i + 1] = ketqua;
+            a.erase(a.begin() + i - 1);
+            a.erase(a.begin() + i - 1);
+            i--;
+        }
+        else if (a[i] == "/")
+        {
+            string ketqua = PhepChia(a[i - 1], a[i + 1]);
+            a[i + 1] = ketqua;
+            a.erase(a.begin() + i - 1);
+            a.erase(a.begin() + i - 1);
+            i--;
+        }
+        len = a.size();
     }
-    
 }
 
-void TimToanTu(char *str, int i)
+void XuLyCongTru(vector<string> &a)
 {
-    int start = i;
-    int stop = i;
-    while (str[start] >= '0' && str[start] <= '9')
+    int len = a.size();
+    for (int i = 0; i < len; i++)
     {
-        start--;
-    }
-    while (str[stop] >= '0' && str[stop] <= '9')
-    {
-        stop++;
-    }
-    
-}
-
-void XuLyNhanChiaTruoc(char *str)
-{
-    int len = STRLEN(str);
-    for (int i = 1; i < len - 2; i++)
-    {
+        if (a[i] == "+")
+        {
+            string ketqua = PhepCong(a[i - 1], a[i + 1]);
+            a[i + 1] = ketqua;
+            a.erase(a.begin() + i - 1);
+            a.erase(a.begin() + i - 1);
+            i--;
+        }
+        else if (a[i] == "-")
+        {
+            string ketqua = PhepTru(a[i - 1], a[i + 1]);
+            a[i + 1] = ketqua;
+            a.erase(a.begin() + i - 1);
+            a.erase(a.begin() + i - 1);
+            i--;
+        }
+        len = a.size();
     }
 }
 
 int main()
 {
-    char str[] = " ";
-    printf("\nDo dai cua string: %d", STRLEN(str));
+    string str = "1 + 2 * 333 - 420 / 5";
+    vector<string> a;
+    AddVector(str, a);
+    XuLyNhanChia(a);
+    XuLyCongTru(a);
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i];
+    }
     return 0;
 }
